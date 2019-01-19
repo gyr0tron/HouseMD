bf50cd419fa56518c82aa5014c748a07a03e6815
nurdtechie98 <nurdtechie98@gmail.com>
2019-01-20 03:49:30 +0530

    unlock metamask

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import web3 from './web3';
 // import { renderRoutes } from 'react-router-config';
 import Loadable from 'react-loadable';
 import './App.scss';
+
+
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "paybills",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_value",
+				"type": "uint256"
+			},
+			{
+				"name": "_receiver",
+				"type": "address"
+			}
+		],
+		"name": "sellHouse",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+]
+
+let contract; 
 
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 
@@ -36,13 +85,25 @@ const Page500 = Loadable({
 class App extends Component {
 
   state = {
-
+    isMetaMask: false
   };
 
-  componentDidMount() {
-    // this.callApi()
-    //   .then(res => this.setState({ response: res.express }))
-    //   .catch(err => console.log(err));
+  async componentDidMount() {
+    if (typeof window.web3 !== 'undefined') {
+        console.log('web3 is enabled');
+        if (web3.currentProvider.isMetaMask === true) {
+            await this.setState({isMetaMask: true});
+            console.log('MetaMask is active');
+        } else {
+            console.log('MetaMask is not available')
+        }
+    } else {
+        console.log('web3 is not found')
+    }
+    if (this.state.isMetaMask) {
+      contract = new web3.eth.Contract(abi, '0x3d3701b458828af7202b4a0bbe77debac64574cd');
+    }
+
   }
 
   // callApi = async () => {
